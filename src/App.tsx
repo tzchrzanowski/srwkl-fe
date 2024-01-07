40c337a0c05e0a91605ca572db37d4520f024ee3
tzchrzanowski <tzchrzanowski@gmail.com>
2024-01-07 13:49:44 +0100

    initial landing page styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import logoSRWKL from './res/logo_crests_only.png'
+import logoCaption from './res/logo_caption_only.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logoSRWKL} className="App-logo" alt="logo" />
+        <img src={logoCaption} className="App-logo-caption" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Stowarzyszenie Rodów Wielkiego Księstwa Litewskiego
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
