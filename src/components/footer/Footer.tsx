740c5cd08153d2ddda5ca710e80b10fd9e622444
tzchrzanowski <tzchrzanowski@gmail.com>
2024-01-17 05:31:50 +0100

    added spinning logo in header

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
     const { t } = useTranslation();
 
     return (
-        <div className={"footer-wrapper fb fb-row jc-space-between ai-center w100 h-30p"}>
+        <div className={"footer-wrapper fb fb-row jc-space-between ai-center w100vw h-30p"}>
             <span className={"ml-3"}>{t('footer.copyright')}</span>
             <div className={"mr-3"}>
                 <span className={"ml-1 mr-1"}>{t('footer.statute-pl')}</span>
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import "./Header.css";
 import Logo from 'res/logo_srwkl.png';
 import {useTranslation} from "react-i18next";
-
+import logoSRWKL from "res/logo_crests_only.png";
+import logoCaption from "res/logo_caption_only.png";
 
 export function Header() {
     const { t } = useTranslation();
 
     return (
         <div className={"header-wrapper fb fb-row jc-center ai-center mt-3"}>
-            <img src={Logo} alt={"logo"} height={"250"} />
-            <div className={"fb fb-col ai-center jc-flex-start"}>
+            {/*<img src={Logo} alt={"logo"} height={"250"} />*/}
+            <img src={logoSRWKL} className="header-logo" alt="logo" height={"250"}/>
+            <img src={logoCaption} className="header-logo header-logo-caption" alt="logo" height={"250"} />
+            <div className={"w200"}></div>
+            {/*<div className={"w500 fb fb-col ai-flex-start jc-flex-start ml-7"}>*/}
+            <div className={"fb fb-col ai-flex-start jc-flex-start ml-7"}>
                 <div>
                     <span className={"font-xxl"}>{t('header.s')}</span>
                     <span className={"font-xl"}>{t('header.stowarzyszenie')}</span>
