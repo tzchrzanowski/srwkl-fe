9eee5ad4c350df3d815d37e52ec2ccf2350b9879
tzchrzanowski <tzchrzanowski@gmail.com>
2024-01-21 04:01:13 +0100

    added board page translations and board list

--- src/data/static-values.ts
+++ src/data/static-values.ts
@@ -3,3 +3,19 @@ export const nip = "9512515999";
 export const bankAccNumber = "72 1160 2202 0000 0004 9410 4801";
 export const emailAddress = "55zadora@gmail.com";
 export const phoneNumber = "(+48) 662-549-616";
+
+export const board = [
+    {
+        name: "Iwona Zawisza-Chrzanowska",
+        role: "president"
+    },
+    {
+        name: "Zofia Okrasso",
+        role: "member"
+    },
+    {
+        name: "Michał Rudnicki",
+        role: "member"
+    }
+];
+
