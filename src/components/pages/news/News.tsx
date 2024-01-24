ffc46c454afb39f31766c495dc3b453ebf9c804b
tzchrzanowski <tzchrzanowski@gmail.com>
2024-01-24 07:39:53 +0100

    added content to news page

--- src/components/pages/news/News.tsx
+++ src/components/pages/news/News.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import {useTranslation} from "react-i18next";
 import TopNavigation from "components/top-navigation/TopNavigation";
 import Header from "components/header/Header";
-import PanTadeuszImg from "res/img/Pan_Tadeusz_rody.jpg";
 import Footer from "components/footer/Footer";
+import { news } from "data/news";
 
 export function News() {
     const {t} = useTranslation();
@@ -16,6 +16,23 @@ export function News() {
                 <div className={"fb fb-col"}>
                     <div className={"fb fb-col jc-center ml-1 mr-1 mt-4 mb-4 max-vw-responsive"}>
                         <span className={"font-l font-b"}>{t("news.header")}</span>
+                        {news && news.map((el, id)=> {
+                            return (
+                                <div className={"fb fb-col mt-4"} key={id}>
+                                    <span className={"font-b font-l"}>{el.header}</span>
+                                    <span className={"font-m mt-2"}>{el.caption}</span>
+                                    {el.urlCaption ?
+                                        <a href={el.url} className={"font-m mt-2"} target={"_blank"}>{el.urlCaption}</a>
+                                        : ""
+                                    }
+                                    <img className={"mt-2"} src={el.img}/>
+                                    {el.img2 ?
+                                        <img className={"mt-2"} src={el.img2}/>
+                                        : ""
+                                    }
+                                </div>
+                            );
+                        })}
                     </div>
                 </div>
             </div>
---
+++ src/data/news.ts
@@ -0,0 +1,32 @@
+import Listopad11Img from 'res/news_images/11_listopada.jpg';
+import KoncertImg from 'res/news_images/koncert.jpg';
+import Christmas2023plImg from 'res/news_images/christmas_2023_pl.jpg';
+import Christmas2023ltImg from 'res/news_images/christmas_2023_lt.jpg';
+
+export const news = [
+    {
+        header: "",
+        caption: "Radosnych Świąt Bożego Narodzenia\n\n i Do Siego Roku 2024 życzy\n\nStowarzyszenie Rodów Wielkiego Księstwa Litewskiego",
+        img: Christmas2023plImg,
+        img2: Christmas2023ltImg
+    },
+    {
+        header: "Koncert Łazienki Królewskie",
+        caption: "apraszamy na koncert legendarnego saksofonisty Petrasa Vyšniauskasa i znanej pianistki Aleksandry Žvirblyte, którzy od ponad 5 lat wspólnie wykonują utwory litewskich i zagranicznych kompozytorów.\n" +
+            "Koncert Dedykacja dla Litwy odbędzie 14 listopada o godz. 17.00 w Pałacu na Wyspie.\n" +
+            "\n" +
+            "Program nawiązuje do wielonarodowości i wielokulturowości Litwy. Usłyszymy utwory kompozytów różnych wyznań i narodowości, powstałe od XVII wieku do dziś. Będzie to wspaniała okazja, by zapoznać się z wielonarodową muzyczną tradycją tego państwa oraz zagłębić się we wspólnej, polsko-litewskiej historii. Organizatorem koncertu są Muzeum Łazienki Królewskie oraz Lietuvos ambasada Lenkijoje / Ambasada Litwy w Polsce Lietuvos ambasada Lenkijoje / Ambasada Litwy w Polsce\n" +
+            "Bilety w cenie 17 zł można kupić online: Stronie Kicket lub w kasach Muzeum. Liczba miejsc ograniczona.\n" +
+            "Zdjęcie: Benedykt Henryk Tyszkiewicz, Autoportret z Isabelle Feraud Wiała, 1893. Kolekcja Gražiny i Gediminasa Petraitisów",
+        img: KoncertImg
+    },
+    {
+        header: "#11Listopada",
+        caption: "Serdecznie pozdrawiamy wszystkich Polaków z okazji Święta Niepodległości! Naszym sąsiadom życzymy wszelkiej pomyślności oraz dobrobytu.\n" +
+            "#LTPLlinks\n" +
+            "#11listopada\n",
+        urlCaption: "Lenkijos Respublikos ambasada Vilniuje - Ambasada RP w Wilnie",
+        url: "https://www.facebook.com/groups/494044345108609/user/100071753506238/",
+        img: Listopad11Img
+    }
+];
